b6349852bbc04cfdf1f41dc19324637d32e15bd3
Joe Shaffer <jjshafferjr@gmail.com>
2020-12-04 14:53:40 -0600

    Add files via upload

--- CombineSubjectData/iterativeClusterSubmit.sh
+++ CombineSubjectData/iterativeClusterSubmit.sh
@@ -1,1 +1,57 @@
+#!/bin/bash
+
+#Set the name of the job. This will be the first part of the error/output filename.
+
+#$ -N SCZ_T1rho_CombineData
+
+#Set the shell that should be used to run the job.
+#$ -S /bin/bash
+
+#Set the current working directory as the location for the error and output files.
+#(Will show up as .e and .o files)
+#$ -cwd
+
+#Select the queue to run in
+#$ -q PINC
+
+#Select the number of slots the job will use
+#$ -pe smp 4 
+
+#Print informationn from the job into the output file
+#/bin/echo Here I am: `hostname`. Sleeping now at: `date`
+#/bin/echo Running on host: `hostname`.
+#/bin/echo In directory: `pwd`
+#/bin/echo Starting on: `date`
+
+#Send e-mail at beginning/end/suspension of job
+#$ -m n
+
+#E-mail address to send to
+#$ -M joseph-shaffer@uiowa.edu
+
+#Run as Array Job
+#$ -t 1:1:1
+
+#Do Stuff
+
+module load matlab/2018a
+
+cd /Shared/MRRCdata/SCZ_TMS_TIMING/scripts/T1rhoPipeline/CombineSubjectData
+
+
+
+COVAR_FILE='SCZ_TMS_SessionList-03-Dec-2020.txt'
+DATA_DIR='/Shared/MRRCdata/SCZ_TMS_TIMING/derivatives/T1rho'
+OUTPREFIX='SCZ_TMS_T1rho_data'
+
+matlab -nodesktop -nosplash -r "createDataFile($COVAR_FILE, $DATA_DIR, $OUTPREFIX);quit;"
+
+COVAR_FILE='SCZvHC_SessionList-03-Dec-2020.txt'
+DATA_DIR='/Shared/MRRCdata/SCZ_TMS_TIMING/derivatives/T1rho'
+OUTPREFIX='SCZvHC_T1rho_data'
+
+matlab -nodesktop -nosplash -r "createDataFile($COVAR_FILE, $DATA_DIR, $OUTPREFIX);quit;"
+
+
+
 
